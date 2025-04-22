9feeaa2f49e7a168cb6902a1fceac639d1d96025
HeyKaetzchen <hinterleitnermarlies@gmail.com>
2025-04-22 15:10:36 +0200

    #60 add navigation bar correct

---
+++ lib/features/home/view/plant_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PlantScreen extends StatelessWidget {
+  const PlantScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("View your Plants! 🌿")),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,85 @@
 import 'package:botanicare/features/home/view/home_screen.dart';
-import 'package:botanicare/features/home/viewmodel/home_screen_view_model.dart';
+import 'package:botanicare/features/home/view/plant_screen.dart';
+import 'package:botanicare/features/home/view/room_screen.dart';
+import 'package:botanicare/features/home/view/settings_screen.dart';
+import 'package:botanicare/features/home/view/task_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const BotaniCareMobileApp());
 }
 
+
 class BotaniCareMobileApp extends StatelessWidget {
   const BotaniCareMobileApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'BotaniCare',
+      home: BotaniCareHome(),
+    );
+  }
+}
+
+class BotaniCareHome extends StatefulWidget {
+  const BotaniCareHome({super.key});
+
+  @override
+  BotaniCareHomeState createState() => BotaniCareHomeState();
+}
+
+class BotaniCareHomeState extends State<BotaniCareHome> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    PlantScreen(),
+    TasksScreen(),
+    RoomScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BotaniCare'),
       ),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => HomeScreenViewModel())
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: ,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_florist),
+            label: 'Plants',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task_alt),
+            label: 'Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.weekend),
+            label: 'Rooms',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
         ],
-        child: const HomeScreen(),
-      )
+      ),
     );
   }
 }
