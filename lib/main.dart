689df3ef34c28f24fa2daab6f1b71f4d84d60d7e
theinisch <t.heinisch@aka-grills.de>
2025-04-22 11:27:36 +0200

    feat: material design 3 theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:botanicare/features/home/view/home_screen.dart';
 import 'package:botanicare/features/home/viewmodel/home_screen_view_model.dart';
+import 'package:botanicare/themes/text_theme.dart';
+import 'package:botanicare/themes/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,16 +14,23 @@ class BotaniCareMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+
+    // Retrieves the default theme for the platform
+    //TextTheme textTheme = Theme.of(context).textTheme;
+
+    // Use with Google Fonts package to use downloadable fonts
+    TextTheme textTheme = createTextTheme(context, "Inter Tight", "Inter");
+    MaterialTheme theme = MaterialTheme(textTheme);
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      themeMode: ThemeMode.system,
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
       home: MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => HomeScreenViewModel())
         ],
-        child: const HomeScreen(),
+        child: const Scaffold(body: HomeScreen()),
       )
     );
   }
