1f4cc116fb54d2cdf1131e5fe6341f1619a5071c
quynh.vu <quynh.vu@kbst-gmbh.de>
2025-04-28 10:47:43 +0200

    #11 display plants in plant screen

--- lib/features/home/view/plant_screen.dart
+++ lib/features/home/view/plant_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/plant_card.dart';
+
 class PlantScreen extends StatelessWidget {
   const PlantScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("View your Plants! 🌿")));
+    return Scaffold(
+      body: GridView.count(
+        padding: const EdgeInsets.all(8.0),
+        crossAxisCount: 2,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+        childAspectRatio: 0.8,
+        children: List.generate(
+          7,
+          (index) => PlantCard(
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2022/08/05/18/50/houseplant-7367379_1280.jpg",
+          ),
+        ),
+      ),
+    );
   }
 }
