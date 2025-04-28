e958dc83ca38c09138a5f69af1b07fd40cf4b4f5
quynh.vu <quynh.vu@kbst-gmbh.de>
2025-04-28 11:18:31 +0200

    #14 added popup menu button to card for delete and edit functionality

--- lib/features/home/widgets/plant_card.dart
+++ lib/features/home/widgets/plant_card.dart
@@ -10,19 +10,45 @@ class PlantCard extends StatelessWidget {
     return Card(
       elevation: 4,
       color: Colors.white,
-      margin: EdgeInsets.all(16),
+      margin: EdgeInsets.all(12),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-            child: Image.network(
-              imageUrl,
-              width: double.infinity,
-              height: 115,
-              fit: BoxFit.cover,
-            ),
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+                child: Image.network(
+                  imageUrl,
+                  width: double.infinity,
+                  height: 120,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: 1,
+                right: 1,
+                child: PopupMenuButton<String>(
+                  onSelected: (value) {
+                    if (value == "bearbeiten") {
+                      //edit functionality - redirect to edit form
+                    } else if (value == "löschen") {
+                      //delete functionality
+                    }
+                  },
+                  itemBuilder:
+                      (context) => [
+                        PopupMenuItem(
+                          value: "bearbeiten",
+                          child: Text("Bearbeiten"),
+                        ),
+                        PopupMenuItem(value: "löschen", child: Text("Löschen")),
+                      ],
+                  icon: Icon(Icons.more_vert, size: 25, color: Colors.white),
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(11, 11, 11, 5),
