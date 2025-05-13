27a42c051c4f2703c3bd6b14230585ec9c15b370
mxw02 <maurice.woike@gmail.com>
2025-05-13 15:03:23 +0200

    BOCA-39: Changes android deploy mechanism

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ import 'package:botanicare/features/home/view/plant_screen.dart';
 import 'package:botanicare/features/home/view/room_screen.dart';
 import 'package:botanicare/features/home/view/settings_screen.dart';
 import 'package:botanicare/features/home/view/task_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'features/home/viewmodel/task_provider.dart';
 import 'features/home/viewmodel/task_screen_view_model.dart';
@@ -15,7 +18,22 @@ import 'features/home/viewmodel/plant_provider.dart';
 final GlobalKey<NavigatorState> navigatorStateRoom =
     GlobalKey<NavigatorState>();
 
-void main() {
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print("Background Message: ${message.messageId}");
+}
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const BotaniCareMobileApp());
 }
 
